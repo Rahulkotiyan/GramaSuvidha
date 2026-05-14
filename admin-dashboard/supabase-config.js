df421576e68de75c2c5aa127b4e3018f29850bcc
Rahulkotiyan <143371571+Rahulkotiyan@users.noreply.github.com>
2026-05-14 19:48:35 +0530

    gitignore update

--- admin-dashboard/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://uqgeyezfbcwxfoksglrd.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxZ2V5ZXpmYmN3eGZva3NnbHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzODg2OTgsImV4cCI6MjA5Mzk2NDY5OH0.VfaJg4O3FF1FWJpR9VZZ2MuAVLUBMv_PmlJXCajOtfI";
-
-// Initialize the Supabase client
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
